dfc4983202a48529b4e71e4e4fb856901a5abaab
Serkan Aksoy <serkanaksy@pm.me>
2025-05-28 17:50:30 +0300

    NHook init

---
+++ include/nhook.h
@@ -0,0 +1,38 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Serkan Aksoy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __NHOOK_H__
+#define __NHOOK_H__
+
+#ifdef NHOOK_EXPORTS
+#define NHOOK_API __declspec(dllexport)
+#else
+#define NHOOK_API __declspec(dllimport)
+#endif
+
+#include "ntosutils.h"
+
+void NHOOK_API test();
+
+#endif // !__NHOOK_H__
